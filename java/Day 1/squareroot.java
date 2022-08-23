6a9a5450f60d85f489ca5db82945d762f6b4ae85
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-08-23 16:34:34 +0530

    Create squareroot.java

---
+++ java/Day 1/squareroot.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+class squareroot{
+    public static void main(String[] args ){
+        int a=20;
+        double c=Math.sqrt(a);
+        System.out.println("The square root is "+c);
+    }
+}
