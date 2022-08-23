12ee7f720a1f1883540d0c71d4d59549eba9515f
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-08-23 17:01:57 +0530

    Create Rectangle.java

---
+++ java/Day 1/rectangle/Rectangle.java
@@ -0,0 +1,16 @@
+public class Rectangle{
+    int length;
+    int breadth;
+    int area;
+    Rectangle(int length,int breadth){
+        this.length=length;
+        this.breadth=breadth;
+        //default constructor
+        this.area=this.setarea();
+    }
+    private int setarea(){
+        //private method used in constructor
+        area=length*breadth;
+        return area;
+    }
+}
