8c1b0eb1e65a271814a88380747cb0f830746a87
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-08-30 15:59:03 +0530

    Create complex.java

---
+++ Java-practicals/Exp1/question2/complex.java
@@ -0,0 +1,13 @@
+public class complex {
+   double real;
+   double img;
+
+  complex(double real, double img) {
+    this.real = real;
+    this.img = img;
+  }
+
+  static void print(complex a) { //static method
+    System.out.println(a.real + "+" + a.img + "i");
+  }
+}
