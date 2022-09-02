e6ff1d5b8839ddc191531a552a359271c05ec91b
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-02 11:55:33 +0530

    Create Main.java

---
+++ Java-practicals/Exp 2/Extra/Main.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Make class vector with methods to add and take product.
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    try{
+	    Scanner my_Scanner= new Scanner(System.in);
+	    
+ vector A = new vector(my_Scanner.nextDouble(),my_Scanner.nextDouble(),my_Scanner.nextDouble());
+ vector B =  new vector(my_Scanner.nextDouble(),my_Scanner.nextDouble(),my_Scanner.nextDouble());
+ vector C = vector.sum(A,B);
+ C.print();
+ double c= vector.dot_product(A,B);
+ System.out.println(c);
+  C = vector.cross_product(A,B);
+ C.print();
+ 
+	    C = new vector();
+	    
+ }
+ catch (EmptyVectorException ex){
+     
+      System.out.println(ex.getMessage());  //print error message
+ }
+	}
+}
