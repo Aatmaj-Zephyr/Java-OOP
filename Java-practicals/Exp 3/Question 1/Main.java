8bb4b87ed287338bd39d6538ce39f73614fc50ab
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-06 15:54:04 +0530

    Create Main.java

---
+++ Java-practicals/Exp 3/Question 1/Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+implement power and factorial in java using static methods.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		double c=myMath.power(3,3);
+		System.out.println(c);
+		double d=myMath.factorial(7);
+		System.out.println(d);
+			double f=myMath.exp(4);
+		System.out.println(f);
+		
+			double g=myMath.one_minus_x(0.5);
+		System.out.println(g);
+	}
+}
