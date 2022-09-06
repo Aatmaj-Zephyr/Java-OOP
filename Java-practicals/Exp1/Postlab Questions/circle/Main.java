87d70087fcf63fb418ed857776d83d03b0e7fb0a
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-06 15:29:20 +0530

    Create Main.java

---
+++ Java-practicals/Exp1/Postlab Questions/circle/Main.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+Write a program to find the area and circumference of a circle using two classes.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		circle A = new circle(3.0);
+		System.out.println(A.circumference());
+			System.out.println(A.area());
+	}
+}
