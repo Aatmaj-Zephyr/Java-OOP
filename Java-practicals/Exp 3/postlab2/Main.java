bfcb829db52d2c7ce069b06abff64e4431ef0d09
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-16 11:58:11 +0530

    Create Main.java

---
+++ Java-practicals/Exp 3/postlab2/Main.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+Write a program to create an array where ith row has i columns.
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    	int len=3;
+	    int a[][]=new int [len][];
+
+	for (int i=0;i<len;i++){
+	    a[i]= new int[i];
+	}
+	}
+}
