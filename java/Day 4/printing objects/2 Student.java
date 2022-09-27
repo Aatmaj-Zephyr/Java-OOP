44707f2a2f9c34831bd09346a2758cd6ab567593
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-27 04:05:38 -0700

    Create 2 Student.java

---
+++ java/Day 4/printing objects/2 Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int rollno;
+    String name;
+    Student(int rollno,String name){
+        this.rollno=rollno;
+        this.name=name;
+    }
+    public String toString(){
+        return name;
+    }
+}
