32c2531ff9b3ad564cc417dc4fbbae49a7c42ae0
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-09-27 03:55:32 -0700

    Create Main.java

---
+++ java/Day 4/printing objects/Main.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+Create class student with int rollno and string name.
+Create 2 objects and print objects through output stream.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Student s1 = new Student(2,"Suleman");
+		Student s2 = new Student(3,"Shambhu");
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
